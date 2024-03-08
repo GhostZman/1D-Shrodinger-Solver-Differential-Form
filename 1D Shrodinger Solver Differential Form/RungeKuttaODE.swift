aad48aac824c24c075a9205c9988edc65d17c226
Zachary Ives <zives@hawk.iit.edu>
2024-03-08 15:59:40 -0600

    setup

--- 1D Shrodinger Solver Differential Form/RungeKuttaODE.swift
+++ 1D Shrodinger Solver Differential Form/RungeKuttaODE.swift
@@ -10,4 +10,26 @@ import Observation
 
 @Observable class RungeKuttaODE {
     
+    var k1: Double = 0.0
+    var k2: Double = 0.0
+    var k3: Double = 0.0
+    var k4: Double = 0.0
+    
+    func findK(order: Int, function: () -> Double){
+        if order > 1 {
+            findK(order: order - 1, function: function)
+        }
+        
+    }
+    func rk0() {
+        
+    }
+    
+    func rk2() {
+        
+    }
+    
+    func rk4() {
+        
+    }
 }
