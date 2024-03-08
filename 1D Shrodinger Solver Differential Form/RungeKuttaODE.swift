31573697847303d02028821e773c664f15f1549b
Zachary Ives <zives@hawk.iit.edu>
2024-03-08 14:25:21 -0600

    Made RungeKutta class

---
+++ 1D Shrodinger Solver Differential Form/RungeKuttaODE.swift
@@ -0,0 +1,13 @@
+//
+//  Runge-Kutta ODE.swift
+//  1D Shrodinger Solver Differential Form
+//
+//  Created by Phys440Zachary on 3/8/24.
+//
+
+import Foundation
+import Observation
+
+@Observable class RungeKuttaODE {
+    
+}
